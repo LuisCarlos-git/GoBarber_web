54abaf2ea74ae4f5b79a6b4d2f75bda513222076
luis carlos <luisestrela.bol951@gmail.com>
2020-04-01 18:50:38 -0300

    create and configure routes

---
+++ src/routes/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Switch } from 'react-router-dom';
+import Route from './Route';
+
+import SingIn from '../pages/SignIn';
+import SingUp from '../pages/SignUp';
+
+import Profile from '../pages/Profile';
+import Dashboard from '../pages/Dashboard';
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={SingIn} />
+      <Route path="/register" component={SingUp} />
+
+      <Route path="/profile" component={Profile} isPrivate />
+      <Route path="/dashboard" component={Dashboard} isPrivate />
+    </Switch>
+  );
+}
